44cea496f873f22130bfca758bb88635c20383a2
BohdanQQ <40754203+BohdanQQ@users.noreply.github.com>
2022-07-18 16:45:50 +0200

    update certificate generation

--- server/certs/remote/server_cert_gen.sh
+++ server/certs/remote/server_cert_gen.sh
@@ -3,14 +3,16 @@
 set -eux
 
 # creates multi-purpose (server & client) locally-signed certificate for this IVIS-core instance
-# usage: server_cert_gen.sh OUTNAME CNAME
+# usage: server_cert_gen.sh OUTNAME IVISNAME SBOXNAME APINAME 
 # if you would like to use this certificate as the referse proxy server cert, edit your /etc/hosts or 
 #corresponding Windows hosts file to have your CNAME point to localhost
 
 # OUTNAME.pem, OUTNAME.key files will be generated
 
 OUT_NAME=$1
-EXECUTOR_NAME=$2
+IVIS_NAME=$2
+SBOX_NAME=$3
+API_NAME=$4
 CNF_FILE=$(mktemp gen-server-cert-XXXXXXX --suffix=.cnf)
 
 echo "
@@ -27,7 +29,9 @@ extendedKeyUsage       = serverAuth, clientAuth, emailProtection
 subjectAltName         = @alt_names
 
 [alt_names]
-DNS.1                  = ${EXECUTOR_NAME}
+DNS.1                  = ${IVIS_NAME}
+DNS.2                  = ${SBOX_NAME}
+DNS.3                  = ${API_NAME}
 " > "${CNF_FILE}"
 
 KEY_NAME="${OUT_NAME}.pem"
@@ -35,7 +39,7 @@ REQ_NAME="${OUT_NAME}.csr"
 CRT_NAME="${OUT_NAME}.cert"
 
 openssl genrsa -out "${KEY_NAME}" 4096
-openssl req -new -key "${KEY_NAME}" -out "${REQ_NAME}" -subj "/CN=${EXECUTOR_NAME}"
+openssl req -new -key "${KEY_NAME}" -out "${REQ_NAME}" -subj "/CN=${IVIS_NAME}"
 openssl x509 -req -in "${REQ_NAME}" -CA ./ca/ca.pem -CAkey ./ca/ca.key -out "${CRT_NAME}" -CAcreateserial -days 9999 -sha256 -extfile "${CNF_FILE}" -extensions v3_req
 
 rm "${REQ_NAME}" "${CNF_FILE}"
