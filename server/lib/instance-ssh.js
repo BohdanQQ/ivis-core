a3901fb46f5d4663ad702c7088cb99500d3dfac3
BohdanQQ <40754203+BohdanQQ@users.noreply.github.com>
2022-11-22 01:11:17 +0100

    update instance-ssh, add ssh2 dependency

--- server/lib/instance-ssh.js
+++ server/lib/instance-ssh.js
@@ -1,5 +1,8 @@
 const config = require('./config');
+const log = require('./log');
+const LOG_ID = 'instance-ssh';
 const fs = require('fs');
+const { Client } = require('ssh2');
 
 const PRIVATE_KEY_PATH = config.ssh.privatePath;
 const PUBLIC_KEY_PATH = config.ssh.publicPath;
@@ -14,20 +17,92 @@ function getPublicSSHKey() {
  * @returns {string}
  */
 function getPrivateSSHKey() {
-    return fs.readFileSync(PRIVATE_KEY_PATH).toString();
+    return fs.readFileSync(PRIVATE_KEY_PATH);
 }
 
-function getAuthorizedKeyFormat(user) {
-    const pubKey = getPublicSSHKey();
-    const oneLinePubKey =
-        pubKey
-            .replace('-----BEGIN PUBLIC KEY-----', '')
-            .replace('-----END PUBLIC KEY-----', '')
-            .replace('ssh-rsa ', '')
-            .replace(/[\n\r]/g, '');
-    return `ssh-rsa ${oneLinePubKey} ${user}`;
+/**
+ * 
+ * @param {String} command 
+ * @param {String} host 
+ * @param {Number} port 
+ * @param {String} username 
+ * @returns {Promise<{stdout: [String], stderr: [String], error: Error | undefined}>}
+ */
+async function executeCommand(command, host, port, username) {
+    return new Promise((resolve, reject) => {
+        const conn = new Client();
+        let stdout = [];
+        let stderr = [];
+        try {
+            conn.on('ready', () => {
+                conn.exec(command, (err, stream) => {
+                    if (err) reject(err);
+                    stream.on('close', (code, signal) => {
+                        conn.end();
+                        if (code == 0) {
+                            resolve({
+                                stdout,
+                                stderr
+                            });
+                        }
+                        else {
+                            reject({
+                                stdout,
+                                stderr,
+                                error: new Error(`Stream closed with code ${code} and signal ${signal}`)
+                            });
+                        }
+                    }).on('data', (data) => {
+                        stdout.push(data.toString().trim());
+                    }).stderr.on('data', (data) => {
+                        stderr.push(data.toString().trim());
+                    });
+                });
+            }).on('error', (message) => {
+                reject({
+                    stdout,
+                    stderr,
+                    error: new Error(`Cannot connect: ${message}`)
+                });
+            })
+                .connect({
+                    host,
+                    port,
+                    username,
+                    privateKey: getPrivateSSHKey()
+                });
+        } catch (err) {
+            reject({
+                stdout,
+                stderr,
+                error: err
+            });
+        }
+    });
+}
+
+async function canMakeSSHConnectionTo(host, port, username) {
+    return new Promise((resolve) => {
+        const conn = new Client();
+        log.verbose(LOG_ID, `trying ${username}@${host}:${port}`);
+        conn.on('ready', () => {
+            conn.end();
+            log.verbose(LOG_ID, `Connection estabilished`);
+            resolve(true);
+        })
+            .on('error', (message) => {
+                log.verbose(LOG_ID, `connecting to ${host}:${port}`, message);
+                resolve(false);
+            })
+            .connect({
+                host,
+                port,
+                username,
+                privateKey: getPrivateSSHKey().toString(),
+            });
+    });
 }
 
 module.exports = {
-    getPrivateSSHKey, getPublicSSHKey, getAuthorizedKeyFormat
+    getPublicSSHKey, executeCommand, canMakeSSHConnectionTo
 }
