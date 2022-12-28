dac01a2ffc63911714955a82e5e9b6046d30e136
BohdanQQ <40754203+BohdanQQ@users.noreply.github.com>
2022-12-28 12:51:32 +0100

    cleanup, refactor with sshWrapper

--- server/lib/pools/oci/basic/oci-basic.js
+++ server/lib/pools/oci/basic/oci-basic.js
@@ -1,27 +1,27 @@
+const core = require('oci-core');
 const {
     createNewPoolParameters,
     getGlobalStateForOCIExecType,
-    INSTANCE_SSH_PORT
+    INSTANCE_SSH_PORT,
 } = require('./global-state');
-const core = require("oci-core");
 const {
     virtualNetworkClient, virtualNetworkWaiter, COMPARTMENT_ID, TENANCY_ID, identityClient,
-    computeClient, computeWaiter
+    computeClient, computeWaiter,
 } = require('./clients');
 const certs = require('../../../remote-certificates');
 const log = require('../../../log');
-const { getPublicSSHKey, executeCommand, canMakeSSHConnectionTo } = require("../../../instance-ssh");
-const knex = require("../../../knex");
+const { getPublicSSHKey, sshWrapper, canMakeSSHConnectionTo } = require('../../../instance-ssh');
+const knex = require('../../../knex');
 const { getRJRSetupCommands, getRPSSetupCommands } = require('./rjr-setup');
 const config = require('../../../config');
+
 const LOG_ID = 'ocibasic-pool-creator';
 
 const POOL_PEER_OS = 'Oracle Linux';
-function getSubnetDisplayName(executorId) { return `IVIS-executor-${executorId}-subnet` }
-function getInstanceDisplayName(executorId, instanceIndex) { return `IVIS-PEER-ex${executorId}-${instanceIndex}` }
+function getSubnetDisplayName(executorId) { return `IVIS-executor-${executorId}-subnet`; }
+function getInstanceDisplayName(executorId, instanceIndex) { return `IVIS-PEER-ex${executorId}-${instanceIndex}`; }
 
 async function createSubnet(executorId, subnetMask, vcnId, securityListId) {
-
     log.info(LOG_ID, `creating subnet with mask ${subnetMask} in VCN ${vcnId} for the executor ID ${executorId}`);
     const subnetRequest = {
         createSubnetDetails: {
@@ -29,20 +29,20 @@ async function createSubnet(executorId, subnetMask, vcnId, securityListId) {
             compartmentId: COMPARTMENT_ID,
             displayName: getSubnetDisplayName(executorId),
             vcnId,
-            securityListIds: [securityListId]
-        }
+            securityListIds: [securityListId],
+        },
     };
     log.verbose(LOG_ID, 'subnet request', subnetRequest);
 
     const subnetResponse = await virtualNetworkClient.createSubnet(subnetRequest);
     log.info(LOG_ID, `subnet ID created: ${subnetResponse.subnet.id}`);
     const subnetWaitRequest = {
-        subnetId: subnetResponse.subnet.id
+        subnetId: subnetResponse.subnet.id,
     };
 
     await virtualNetworkWaiter.forSubnet(
         subnetWaitRequest,
-        core.models.Subnet.LifecycleState.Available
+        core.models.Subnet.LifecycleState.Available,
     );
 
     return subnetResponse.subnet.id;
@@ -50,7 +50,7 @@ async function createSubnet(executorId, subnetMask, vcnId, securityListId) {
 
 async function getAvailabilityDomain() {
     const response = await identityClient.listAvailabilityDomains({
-        compartmentId: TENANCY_ID
+        compartmentId: TENANCY_ID,
     });
     return response.items[0];
 }
@@ -59,7 +59,7 @@ async function getImageId(shapeName) {
     const request = {
         compartmentId: COMPARTMENT_ID,
         shape: shapeName,
-        operatingSystem: POOL_PEER_OS
+        operatingSystem: POOL_PEER_OS,
     };
 
     const response = await computeClient.listImages(request);
@@ -70,20 +70,20 @@ async function getInstanceRequestDetails(shapeName, instanceName, subnetId, auth
     const domain = await getAvailabilityDomain();
     const sourceDetails = {
         imageId: await getImageId(shapeName),
-        sourceType: "image"
+        sourceType: 'image',
     };
-    let result = {
+    const result = {
         compartmentId: COMPARTMENT_ID,
-        availabilityDomain: domain.name ? domain.name : "",
+        availabilityDomain: domain.name ? domain.name : '',
         shape: shapeName,
         displayName: instanceName,
         sourceDetails,
         createVnicDetails: {
-            subnetId
+            subnetId,
         },
         metadata: {
             ssh_authorized_keys: authorizedSSHKey,
-        }
+        },
     };
     if (shapeName.toLowerCase().indexOf('flex') !== -1) {
         result.shapeConfig = {
@@ -103,8 +103,8 @@ async function createInstance(executorId, instanceIndex, subnetId, params) {
             instanceName,
             subnetId,
             getPublicSSHKey(),
-            params
-        )
+            params,
+        ),
     };
     log.verbose(LOG_ID, 'Instance parameters', instanceRequest);
 
@@ -112,13 +112,13 @@ async function createInstance(executorId, instanceIndex, subnetId, params) {
     log.info(LOG_ID, `Created instance ${instanceResponse.instance.id}`);
 
     const instanceWaitRequest = {
-        instanceId: instanceResponse.instance.id
+        instanceId: instanceResponse.instance.id,
     };
 
     log.info(LOG_ID, `Waiting for the instance ${instanceName} to be available`);
     const getInstanceResponse = await computeWaiter.forInstance(
         instanceWaitRequest,
-        core.models.Instance.LifecycleState.Running
+        core.models.Instance.LifecycleState.Running,
     );
     return getInstanceResponse.instance.id;
 }
@@ -126,14 +126,14 @@ async function createInstance(executorId, instanceIndex, subnetId, params) {
 async function getInstanceVnic(instanceId) {
     const attachments = await computeClient.listVnicAttachments({
         compartmentId: COMPARTMENT_ID,
-        instanceId
+        instanceId,
     });
     if (attachments.items.length === 0) {
         return null;
     }
 
     // each instance gets its vnic, this is OK
-    const vnicId = attachments.items[0].vnicId;
+    const { vnicId } = attachments.items[0];
     return (await virtualNetworkClient.getVnic({ vnicId })).vnic;
 }
 
@@ -149,28 +149,27 @@ async function fallthroughIfError({ error, result }, promiseGenerator) {
 }
 
 /**
- * 
- * @param {Number} execId 
- * @param {Number} peerIndex 
- * @param {String} subnetId 
- * @param {Object} params 
+ *
+ * @param {Number} execId
+ * @param {Number} peerIndex
+ * @param {String} subnetId
+ * @param {Object} params
 * @returns { { error: String | null, result: String | null } }
  */
 async function createPoolPeer(execId, peerIndex, subnetId, params) {
     try {
         return { error: null, result: await createInstance(execId, peerIndex, subnetId, params) };
-    }
-    catch (err) {
+    } catch (err) {
         return { error: err, result: null };
     }
 }
 
 /**
- * 
- * @param {Number} amount 
- * @param {Number} execId 
- * @param {String} subnetId 
- * @param {Object} params 
+ *
+ * @param {Number} amount
+ * @param {Number} execId
+ * @param {String} subnetId
+ * @param {Object} params
  * @returns {[String]}
  */
 async function createPoolPeers(amount, execId, subnetId, params) {
@@ -178,50 +177,45 @@ async function createPoolPeers(amount, execId, subnetId, params) {
         throw new Error(`invalid amount requested: ${amount}`);
     }
     const peerIndicies = new Array(amount).fill(0).map((_, i) => i);
-    let createdInstanceIds = [];
-    const peerPromises = peerIndicies.map((peerIdx) =>
-        createPoolPeer(execId, peerIdx, subnetId, params)
-            // using fallthrough here because I want all the promises to resolve / wait for the peer machine to be created
-            .then((res) => fallthroughIfError(res, (instanceId) => {
-                createdInstanceIds.push(instanceId);
-            })));
+    const createdInstanceIds = [];
+    const peerPromises = peerIndicies.map((peerIdx) => createPoolPeer(execId, peerIdx, subnetId, params)
+    // using fallthrough here because I want all the promises to resolve / wait for the peer machine to be created
+        .then((res) => fallthroughIfError(res, (instanceId) => {
+            createdInstanceIds.push(instanceId);
+        })));
 
-    log.info(LOG_ID, "Waiting for all peer machines to be created");
+    log.info(LOG_ID, 'Waiting for all peer machines to be created');
     const peerCreationResults = await Promise.all(peerPromises);
     if (createdInstanceIds.length !== amount) {
         log.error(LOG_ID, `Length ${createdInstanceIds.length} not matching pool size ${amount}`);
         // TODO? destroyAllOkPeers
-        const firstError = peerCreationResults.filter((result) => result.error)[0].error ||
-            new Error("At least one pool peer was not created yet no error was found!");
+        const firstError = peerCreationResults.filter((result) => result.error)[0].error
+            || new Error('At least one pool peer was not created yet no error was found!');
         throw firstError;
     }
-    log.info(LOG_ID, "Instance IDs of created peers: ", createdInstanceIds);
+    log.info(LOG_ID, 'Instance IDs of created peers: ', createdInstanceIds);
     return createdInstanceIds;
 }
 
 async function waitForSSHConnection(host, port, user, attemptCooldownSecs, timeoutSecs, maxRetryCount) {
-    return new Promise(async (resolve, reject) => {
-        let stop = false;
-        let timeout = setTimeout(() => {
-            stop = true;
-            reject(new Error(`SSH connection to ${host} could not be estabilished in the specified time (${timeoutSecs}s)`));
-        }, timeoutSecs * 1000);
+    let stop = false;
+    const timeout = setTimeout(() => {
+        stop = true;
+    }, timeoutSecs * 1000);
 
-        let retryCount = 0;
-        while (!stop && retryCount <= maxRetryCount) {
-            log.verbose(LOG_ID, `SSH waiter retry ${retryCount}`);
-            if (await canMakeSSHConnectionTo(host, port, user)) {
-                log.verbose(LOG_ID, `SSH waiter SUCCESS ${retryCount}`);
-                clearTimeout(timeout);
-                resolve();
-                return;
-            }
-            // sleep
-            await new Promise((resolveTimeout) => setTimeout(resolveTimeout, attemptCooldownSecs * 1000));
-            retryCount = retryCount + 1;
+    let retryCount = 0;
+    while (!stop && retryCount <= maxRetryCount) {
+        log.verbose(LOG_ID, `SSH waiter retry ${retryCount}`);
+        if (await canMakeSSHConnectionTo(host, port, user)) {
+            log.verbose(LOG_ID, `SSH waiter SUCCESS ${retryCount}`);
+            clearTimeout(timeout);
+            return;
         }
-        reject(new Error(`SSH connection to ${host} could not be estabilished after the specified retries (${maxRetryCount}, cooldown ${attemptCooldownSecs})`));
-    });
+        // sleep
+        await new Promise((resolve) => { setTimeout(resolve, attemptCooldownSecs * 1000); });
+        retryCount += 1;
+    }
+    throw new Error(`SSH connection to ${host} could not be estabilished after the specified retries (${maxRetryCount}, cooldown ${attemptCooldownSecs})`);
 }
 
 function getInstanceSetupCommands(subnetMask) {
@@ -239,7 +233,7 @@ function getInstanceSetupCommands(subnetMask) {
 }
 
 function getRJRInstallationCommands(masterInstancePrivateIp, instancePrivateIp, subnetMask) {
-    let commands = getInstanceSetupCommands(subnetMask);
+    const commands = getInstanceSetupCommands(subnetMask);
     commands.push(...getRJRSetupCommands(masterInstancePrivateIp, instancePrivateIp));
     return commands;
 }
@@ -264,44 +258,30 @@ async function runCommandsOnPeers(instanceIds, executorId, commandGenerator) {
                 const maxRetryCount = 3;
                 let retryNum = 0;
                 while (retryNum <= maxRetryCount) {
-                    const executionResult = await executeCommand(command, vnic.publicIp, INSTANCE_SSH_PORT, user);
-                    if (!executionResult.error) {
+                    if (retryNum > 0) {
+                        log.verbose(LOG_ID, `Retrying the command ${command}`);
+                    }
+                    try {
+                        await sshWrapper({ host: vnic.publicIp, port: INSTANCE_SSH_PORT, username: user }, async (connection) => {
+                            await connection.execute(command);
+                        });
                         break;
-                    }
-                    if (!firstError && executionResult.error) {
-                        firstError = executionResult;
-                    }
-                    retryNum = retryNum + 1;
-                    if (executionResult.error) {
-                        log.error(LOG_ID, `Command: ${command} failed with error:`, executionResult.error);
-                        log.error(LOG_ID, `STDOUT: ${executionResult.stdout}`);
-                        log.error(LOG_ID, `STDERR: ${executionResult.stderr}`);
-                        if (retryNum <= maxRetryCount) {
-                            log.error(LOG_ID, "Retrying the command...");
+                    } catch (err) {
+                        if (!firstError) {
+                            firstError = err;
                         }
+                        retryNum += 1;
+                        log.error(LOG_ID, err);
                     }
                 }
                 if (firstError) {
-                    throw executionResult;
+                    throw firstError;
                 }
             }
         });
         await Promise.all(installationPromises);
-    }
-    catch (error) {
+    } catch (error) {
         // TODO destroyAllOkPeers
-        // handle command execution error
-        if (error.stdout) {
-            log.error(LOG_ID, `ExecutorID: ${executorId}:\nSTDOUT of one of the peers:\n${error.stdout.join('\n')}`);
-        }
-        if (error.stderr) {
-            log.error(LOG_ID, `ExecutorID: ${executorId}:\nSTDERR of one of the peers:\n${error.stderr.join('\n')}`);
-        }
-        if ((error.stdout || error.stderr) && error.error) {
-            throw error.error;
-        }
-
-        // propagate any other error
         throw error;
     }
 }
@@ -315,10 +295,10 @@ async function shutdownInstance() {
 }
 
 function convertParams(params) {
-    let retval = {
-        ...params
+    const retval = {
+        ...params,
     };
-    for (const paramName of ["size", "shapeConfigCPU", "shapeConfigRAM"]) {
+    for (const paramName of ['size', 'shapeConfigCPU', 'shapeConfigRAM']) {
         retval[paramName] = Number(params[paramName]);
         if (retval.size <= 0 || Number.isNaN(retval.size)) {
             throw new Error(`Pool parameter ${paramName} is of invalid value. (${retval})`);
@@ -336,24 +316,24 @@ function convertParams(params) {
  */
 
 /**
- * @param { certificateGenerator } certificateGeneratorFunction 
- * @returns 
+ * @param { certificateGenerator } certificateGeneratorFunction
+ * @returns
  */
 async function createOCIBasicPool(executorId, params, certificateGeneratorFunction) {
-    let retVal = {
+    const retVal = {
         subnetId: null,
         subnetMask: null,
         masterInstanceId: null,
         masterInstanceIp: null,
         masterInstanceSubnetIp: null,
         poolInstanceIds: [],
-        error: null
+        error: null,
     };
     params = convertParams(params);
     try {
         const executorGlobalState = await getGlobalStateForOCIExecType(knex);
         const {
-            subnetMask
+            subnetMask,
         } = await createNewPoolParameters();
         retVal.subnetMask = subnetMask;
         retVal.subnetId = await createSubnet(executorId, subnetMask, executorGlobalState.vcn, executorGlobalState.securityList);
@@ -361,27 +341,27 @@ async function createOCIBasicPool(executorId, params, certificateGeneratorFuncti
         retVal.poolInstanceIds = await createPoolPeers(params.size, executorId, retVal.subnetId, params);
 
         if (!(retVal.poolInstanceIds instanceof Array) || retVal.poolInstanceIds.length <= 0) {
-            log.error(LOG_ID, "Pool instance creation unexpectedly returned ", retVal.poolInstanceIds);
-            throw new Error("No pool peers have been created! Cannot select Master Peer.");
+            log.error(LOG_ID, 'Pool instance creation unexpectedly returned ', retVal.poolInstanceIds);
+            throw new Error('No pool peers have been created! Cannot select Master Peer.');
         }
         retVal.masterInstanceId = retVal.poolInstanceIds[0];
         log.info(LOG_ID, `Master Peer Selected: ${retVal.masterInstanceId}`);
         const vnic = await getInstanceVnic(retVal.masterInstanceId);
         retVal.masterInstanceIp = vnic.publicIp;
         retVal.masterInstanceSubnetIp = vnic.privateIp;
-        log.info(LOG_ID, "Master Instance IPs", {
-            'public': retVal.masterInstanceIp,
-            'private': retVal.masterInstanceSubnetIp
+        log.info(LOG_ID, 'Master Instance IPs', {
+            public: retVal.masterInstanceIp,
+            private: retVal.masterInstanceSubnetIp,
         });
 
-        log.info(LOG_ID, "Installing required software on pool peers");
+        log.info(LOG_ID, 'Installing required software on pool peers');
         await runCommandsOnPeers(retVal.poolInstanceIds, executorId, (instanceIp) => getRJRInstallationCommands(retVal.masterInstanceSubnetIp, instanceIp, retVal.subnetMask));
-        
-        log.info(LOG_ID, "Generating certificates for the master peer");
+
+        log.info(LOG_ID, 'Generating certificates for the master peer');
         await certificateGeneratorFunction(retVal.masterInstanceIp);
-        const peerIPs = await (Promise.all( retVal.poolInstanceIds.map(async (id) => (await getInstanceVnic(id)).privateIp)));
-        log.info(LOG_ID, "Installing additional software on master peer");
-        await runCommandsOnPeers([retVal.masterInstanceId], executorId, () => getRPSInstallationCommands(peerIPs, retVal.masterInstanceIp,retVal.masterInstanceSubnetIp, retVal.subnetMask, executorId));
+        const peerIPs = await (Promise.all(retVal.poolInstanceIds.map(async (id) => (await getInstanceVnic(id)).privateIp)));
+        log.info(LOG_ID, 'Installing additional software on master peer');
+        await runCommandsOnPeers([retVal.masterInstanceId], executorId, () => getRPSInstallationCommands(peerIPs, retVal.masterInstanceIp, retVal.masterInstanceSubnetIp, retVal.subnetMask, executorId));
     } catch (error) {
         // TODO failure recovery - terminate all created peers
         retVal.error = error;
@@ -422,7 +402,3 @@ module.exports = { createOCIBasicPool, shutdownOCIBasicPool, verifyOCIBasicPool 
 //         return fn(executorId);
 //     }
 // }
-
-
-
-
